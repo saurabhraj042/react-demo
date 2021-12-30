c05859819aca2efa59df7cc11f9f5ef6af987132
saurabhraj042 <saurabhraj042@gmail.com>
2021-12-30 19:52:42 +0530

    add color change on click logic

--- src/App.js
+++ src/App.js
@@ -3,13 +3,40 @@ import Box from "./components/Box";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      colorOne: `#FF0000`,
+      colorTwo: `#00FF00`,
+    };
+
+    this.colorChangeHandler = this.colorChangeHandler.bind(this)
+  }
+
+  genRandomColorCode() {
+    return (
+      "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0")
+    );
+  }
+
+  colorChangeHandler(){
+    const randomColorOne = this.genRandomColorCode();
+    const randomColorTwo = this.genRandomColorCode();
+    
+    this.setState({
+      colorOne: randomColorOne,
+      colorTwo: randomColorTwo,
+    })
+  }  
+
   render() {
     return (
       <div className="centered">
         <div>
-          <Box color={`#FF0000`} />
-          <Box color={`#FF0000`} />
-          <button>Change Color</button>
+          <Box color={this.state.colorOne} />
+          <Box color={this.state.colorTwo} />
+          <button onClick={this.colorChangeHandler}>Change Color</button>
         </div>
       </div>
     );
