86034643fd8c493a78f568d05fd96e257ec7e2aa
saurabhraj042 <saurabhraj042@gmail.com>
2021-12-31 11:22:58 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
 import React, { Component } from "react";
-import Box from "./components/Box";
 import "./App.css";
+import ClassBox from "./components/ClassBox";
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      colorOne: `#FF0000`,
-      colorTwo: `#00FF00`,
-    };
-
-    this.colorChangeHandler = this.colorChangeHandler.bind(this);
-  }
-
-  genRandomColorCode() {
-    return (
-      "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0")
-    );
-  }
-
-  colorChangeHandler() {
-    const randomColorOne = this.genRandomColorCode();
-    const randomColorTwo = this.genRandomColorCode();
-
-    this.setState({
-      colorOne: randomColorOne,
-      colorTwo: randomColorTwo,
-    });
-  }
-
   render() {
     return (
-      <div className="centered">
-        <div>
-          <Box color={this.state.colorOne} />
-          <Box color={this.state.colorTwo} />
-          <button onClick={this.colorChangeHandler}>Change Color</button>
-        </div>
+      <div>
+        <ClassBox />
       </div>
     );
   }
---
+++ src/components/ClassBox.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import Box from "./Box";
+
+class ClassBox extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      colorOne: `#FF0000`,
+      colorTwo: `#00FF00`,
+    };
+
+    this.colorChangeHandler = this.colorChangeHandler.bind(this);
+  }
+
+  genRandomColorCode() {
+    return (
+      "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0")
+    );
+  }
+
+  colorChangeHandler() {
+    const randomColorOne = this.genRandomColorCode();
+    const randomColorTwo = this.genRandomColorCode();
+
+    this.setState({
+      colorOne: randomColorOne,
+      colorTwo: randomColorTwo,
+    });
+  }
+
+  render() {
+    return (
+      <div className="centered">
+        <div>
+          <Box color={this.state.colorOne} />{" "}
+          <Box color={this.state.colorTwo} />{" "}
+          <button onClick={this.colorChangeHandler}> Change Color </button>{" "}
+        </div>{" "}
+      </div>
+    );
+  }
+}
+
+export default ClassBox;
